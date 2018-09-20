d2ebe6eff23b121f2ce4345656280fdcf46952dd
zhouxinghai <zhouxinghai@treefinance.com.cn>
2018-09-20 14:41:14 +0800

    no message

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 gradle clean spider-share-domain:upload  spider-share-api:upload spider-share-common:upload  -x test
-gradle clean spider-share-dao:upload  spider-share-service:upload spider-share-web:upload -x test
+gradle clean spider-share-dao:upload  spider-share-service:upload spider-share-web:upload crawler-core:upload -x test
 
 
 
